56e1ab9ba920b8f20d7dd0e6222f04486937ebd8
Sam Abrahams <sjabraha@gmail.com>
2016-01-31 15:42:09 -0800

    Fix print syntax in anchorhub_tests.py

--- tests/anchorhub_tests.py
+++ tests/anchorhub_tests.py
@@ -2,10 +2,10 @@ from nose.tools import *
 import anchorhub
 
 def setup():
-    print "Setting up AnchorHub..."
+    print("Setting up AnchorHub...")
 
 def teardown():
-    print "Tearing down AnchorHub..."
+    print("Tearing down AnchorHub...")
 
 def test_basic():
-    print "I ran!"
+    print("I ran!")
