1eaa4f553a58408b414b114f2e65c8091fb464c3
Sam Abrahams <sjabraha@gmail.com>
2016-02-06 14:59:55 -0800

    Update docstrings for ArmedSwitch class and tests

--- anchorhub/lib/armedswitch.py
+++ anchorhub/lib/armedswitch.py
@@ -1,6 +1,8 @@
 """
 Class file for the ArmedSwitch class
 """
+
+
 class ArmedSwitch(object):
     """
     ArmedSwitch is a boolean switch that must be explicitly re-armed after
@@ -11,12 +13,12 @@ class ArmedSwitch(object):
     def __init__(self, switched=False, armed=True):
         """
         Initializer for ArmedSwitch. Default switched state is False,
-        and ready-to-be switched state set to True
+        and ready-to-be switched state set to True.
+
         :param switched: Boolean. Starting state of the switch
         :param armed: Boolean. Starting state of the armed property. When False,
-        the switch must be re-armed with the arm() method before switching
-        again
-        :return:
+            the switch must be re-armed with the arm() method before switching
+            again
         """
         self._switched = switched
         self._armed = armed
@@ -24,41 +26,46 @@ class ArmedSwitch(object):
     def switch(self, val=None):
         """
         Set the state of the switch. If the armed state is set to False,
-        the function does nothing
+        the function does nothing.
+
         :param val: Boolean. The value to set the switch state to. When None,
-        the switch will be set to the opposite of its current state.
-        :return:
+            the switch will be set to the opposite of its current state.
+        :return: Boolean. Returns True if operation was successful (i.e. the
+            switch was armed before this was called)
         """
         if self._armed:
-            if val == None:
+            if val is None:
                 val = not self._switched
             self._switched = val
             self._armed = False
+            return True
+        else:
+            return False
 
     def arm(self):
         """
-        Sets the armed state of the switch to True
-        :return:
+        Sets the armed state of the switch to True.
         """
         self._armed = True
 
     def disarm(self):
         """
-        Sets the armed state of the switch to False
-        :return:
+        Sets the armed state of the switch to False.
         """
         self._armed = False
 
     def is_switched(self):
         """
-        Returns the current state of the switch
+        Returns the current state of the switch.
+
         :return: Boolean
         """
         return self._switched
 
     def is_armed(self):
         """
-        Returns the currened armed status of the switch
+        Returns the currened armed status of the switch.
+
         :return: Boolean
         """
         return self._armed
--- tests/lib/test_armedswitch.py
+++ tests/lib/test_armedswitch.py
@@ -9,14 +9,15 @@ from anchorhub.lib.armedswitch import ArmedSwitch
 
 def test_init_base():
     """
-    Test basic initalization state
+    Test basic initialization state
     :return:
     """
     s = ArmedSwitch()
     assert s.is_switched() == False
     assert s.is_armed() == True
 
-def test_init_values():
+
+def test_init_params():
     """
     Test initializing ArmedSwitch with different starting values
     :return:
@@ -37,72 +38,78 @@ def test_init_values():
     assert s4.is_switched() == False
     assert s4.is_armed() == True
 
+
 def test_switch_basic():
     """
-    Test for basic switch() functionality
+    Test basic ArmedSwitch.switch() functionality
     :return:
     """
     s = ArmedSwitch()
     assert s.is_switched() == False
-    s.switch()
+    assert s.switch() == True
     assert s.is_switched() == True
     assert s.is_armed() == False
 
+
 def test_switch_only_once():
     """
-    Make sure switch doesn't change twice without re-arming it
+    Make ArmedSwitch doesn't change twice without re-arming it
     :return:
     """
     s = ArmedSwitch()
     assert s.is_switched() == False
-    s.switch()
-    s.switch()
+    assert s.switch() == True
+    assert s.switch() == False
     assert s.is_switched() == True
+
 
 def test_switch_only_once_loop():
     """
-    Attempt to flip switch 100 times
-    Make sure it only ever switches once
+    ArmedSwitch only switches once during a loop of 100 attempts
     :return:
     """
     s = ArmedSwitch()
     assert s.is_switched() == False
+    assert s.switch() == True
     for i in range(0,99):
-        s.switch()
+        assert s.switch() == False
         assert s.is_switched() == True
+
 
 def test_switch_argument():
     """
-    Makes sure switch() arguments work as intended
+    Makes sure ArmedSwitch.switch() arguments work as intended
     :return:
     """
     s1 = ArmedSwitch()
-    s1.switch(False)
+    assert s1.switch(False) == True
     assert s1.is_switched() == False
     assert s1.is_armed() == False
 
     s2 = ArmedSwitch()
-    s2.switch(True)
+    assert s2.switch(True) == True
     assert s2.is_switched() == True
     assert s2.is_armed() == False
 
+
 def test_arm():
     """
-    Makes sure that arm() works
+    Test ArmedSwitch.arm()
     :return:
     """
     s = ArmedSwitch()
-    s.switch()
+    assert s.switch() == True
     assert s.is_armed() == False
     s.arm()
     assert s.is_armed() == True
-    s.switch()
+    assert s.switch() == True
     assert s.is_switched() == False
     assert s.is_armed() == False
 
+
 def test_disarm():
     """
-    Makes sure disarm() works
+    Test ArmedSwitch.disarm()
     :return:
     """
     s = ArmedSwitch()
