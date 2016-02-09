a031c5100750441b7619ede202eeb4cfb3635650
Sam Abrahams <sjabraha@gmail.com>
2016-02-09 01:19:10 -0800

    Add cmdparse.py and tests to go along with it

---
+++ anchorhub/cmdparse.py
@@ -0,0 +1,50 @@
+"""
+cmdparse.py - Functions for command-line argument extracting and parsing
+"""
+
+# Global modules
+import argparse
+import sys
+import os
+from os import path
+
+import anchorhub as ah
+import anchorhub.settings.default_settings as ds
+
+
+def parse_args(args=None):
+    """
+    Parse arguments provided as a list of strings, and return a namespace
+    with parameter names matching the arguments
+
+    :param args: List of strings to be parsed as command-line arguments. If
+    none, reads in sys.argv as the values.
+    :return: a namespace containing arguments values
+    """
+    if args is None:
+        args = sys.argv
+    parser = argparse.ArgumentParser(description=ds.ARGPARSER['description'])
+    parser.add_argument('input', help=ds.ARGPARSE_INPUT['help'])
+    parser.add_argument('output', nargs='?', help=ds.ARGPARSE_OUTPUT['help'],
+                        default=ds.ARGPARSE_OUTPUT['default'])
+    parser.add_argument('-X', '--overwrite', help=ds.ARGPARSE_OVERWRITE['help'],
+                        action='store_true')
+    parser.add_argument('-e', '--extension', nargs='+', default=['.md'],
+                        help=ds.ARGPARSE_EXTENSION['help'])
+    parser.add_argument('-w', '--wrapper', help=ds.ARGPARSE_WRAPPER['help'],
+                        default=ds.ARGPARSE_WRAPPER['default'], )
+    parser.add_argument('-v', '--version', action='version',
+                        version=ah.__version__)
+
+    return parser.parse_args(args)
+
+
+def parse_args_dict(args=None):
+    """
+    Parse arguments provided as a list of strings, and return a dictionary
+    mapping options/flags to provided values
+    :param args: List of strings to be parsed as command-line arguments. If
+    none, reads in sys.argv as the values.
+    :return: a dictionary containing the flags and values as a key/value pair
+    """
+    return vars(parse_args(args))
---
+++ anchorhub/tests/test_cmdparse.py
@@ -0,0 +1,85 @@
+"""
+cmdparse.py unit tests
+
+cmdparse.py:
+http://www.github.com/samjabrahams/anchorhub/compatibility.py
+"""
+
+import anchorhub.cmdparse as cmd
+import anchorhub.settings.default_settings as ds
+
+
+def test_parse_args():
+    """
+    Test cmdparse.parse_args()
+
+    The first three sets of assertions check for all the various flag names,
+    both the long versions (--flag) and the short versions (-f)
+
+    The final set of assertions check the default values
+    """
+    args = ['.', 'output', '-e', '.md', '.rst']
+    pargs = cmd.parse_args(args)
+    assert pargs.input == '.'
+    assert pargs.output == 'output'
+    assert pargs.extension == ['.md', '.rst']
+
+    args2 = ['../bears', '-w', '[--> <--]', '-X']
+    pargs2 = cmd.parse_args(args2)
+    assert pargs2.input == '../bears'
+    assert pargs2.wrapper == '[--> <--]'
+    assert pargs2.overwrite == True
+
+    args3 = ['.', '--overwrite', '--wrapper', '$$$$ $$$$', '--extension',
+             '.txt', '.fbi', '.wat', '.hub']
+    pargs3 = cmd.parse_args(args3)
+    assert pargs3.input == '.'
+    assert pargs3.overwrite == True
+    assert pargs3.wrapper == '$$$$ $$$$'
+    assert pargs3.extension == ['.txt', '.fbi', '.wat', '.hub']
+
+    args4 = ['.']
+    pargs4 = cmd.parse_args(args4)
+    assert pargs4.input == ds.INPUT
+    assert pargs4.output == ds.OUTPUT
+    assert pargs4.wrapper == ds.WRAPPER
+    assert pargs4.overwrite == False
+    assert pargs4.extension == ds.ARGPARSE_EXTENSION['default']
+
+
+def test_parse_args_dict():
+    """
+    Test cmdparse.parse_args_dict()
+
+    The first three sets of assertions check for all the various flag names,
+    both the long versions (--flag) and the short versions (-f)
+
+    The final set of assertions check the default values
+    """
+    args = ['.', 'output', '-e', '.md', '.rst']
+    pargs = cmd.parse_args_dict(args)
+    assert pargs['input'] == '.'
+    assert pargs['output'] == 'output'
+    assert pargs['extension'] == ['.md', '.rst']
+
+    args2 = ['../bears', '-w', '[--> <--]', '-X']
+    pargs2 = cmd.parse_args_dict(args2)
+    assert pargs2['input'] == '../bears'
+    assert pargs2['wrapper'] == '[--> <--]'
+    assert pargs2['overwrite'] == True
+
+    args3 = ['.', '--overwrite', '--wrapper', '$$$$ $$$$', '--extension',
+             '.txt', '.fbi', '.wat', '.hub']
+    pargs3 = cmd.parse_args_dict(args3)
+    assert pargs3['input'] == '.'
+    assert pargs3['overwrite'] == True
+    assert pargs3['wrapper'] == '$$$$ $$$$'
+    assert pargs3['extension'] == ['.txt', '.fbi', '.wat', '.hub']
+
+    args4 = ['.']
+    pargs4 = cmd.parse_args_dict(args4)
+    assert pargs4['input'] == ds.INPUT
+    assert pargs4['output'] == ds.OUTPUT
+    assert pargs4['wrapper'] == ds.WRAPPER
+    assert pargs4['overwrite'] == False
+    assert pargs4['extension'] == ds.ARGPARSE_EXTENSION['default']
