5efddf26176ac778556a3568bf97c2e70daac866
Sam Abrahams <sjabraha@gmail.com>
2016-02-09 00:36:40 -0800

    Replace many double quotes with single quotes

--- anchorhub/settings/default_settings.py
+++ anchorhub/settings/default_settings.py
@@ -2,30 +2,30 @@
 Defaults for all settings used by AnchorHub
 """
 
-WRAPPER = "{ }"
+WRAPPER = '{ }'
 
-INPUT = "."
-OUTPUT = "out-anchorhub"
+INPUT = '.'
+OUTPUT = 'out-anchorhub'
 
 ARGPARSER = {
-    "description": "anchorhub parses through Markdown files and precompiles "
+    'description': "anchorhub parses through Markdown files and precompiles "
                    "links to specially formatted anchors."
 }
 ARGPARSE_INPUT = {
-    "help": "Path of directory tree to be parsed",
+    'help': "Path of directory tree to be parsed",
 }
 ARGPARSE_OUTPUT = {
-    "help": "Desired output location (default is \"" + OUTPUT + "\")",
-    "default": OUTPUT
+    'help': "Desired output location (default is \"" + OUTPUT + "\")",
+    'default': OUTPUT
 }
 ARGPARSE_OVERWRITE = {
-    "help": "Overwrite input files; ignore output location"
+    'help': "Overwrite input files; ignore output location"
 }
 ARGPARSE_EXTENSION = {
-    "help": "Indicate which file extensions to search and run anchorhub on.",
-    "default": [".md"]
+    'help': "Indicate which file extensions to search and run anchorhub on.",
+    'default': [".md"]
 }
 ARGPARSE_WRAPPER = {
-    "help": "Specify custom wrapper format (default is \"" + WRAPPER + "\")",
-    "default": WRAPPER
+    'help': "Specify custom wrapper format (default is \"" + WRAPPER + "\")",
+    'default': WRAPPER
 }
