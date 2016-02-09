fdfe141ba0b19f1bdc010fb37374f76f2228bc04
Sam Abrahams <sjabraha@gmail.com>
2016-02-09 00:11:21 -0800

    Update anchorhub/__init__.py to fix __version__
    __version__ will automatically be imported when importing anchorhub.*

--- anchorhub/__init__.py
+++ anchorhub/__init__.py
@@ -1,3 +1,10 @@
 """
 AnchorHub - a Markdown pre-compiler for utilizing GitHub's auto-generated anchors
 """
+
+__all__ = ['__version__']
+
+# AnchorHub version
+import pkgutil
+__version__ = pkgutil.get_data(__package__, 'VERSION').decode('ascii').strip()
+del pkgutil
