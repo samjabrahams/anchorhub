34bd8a111dbbb60d9065d76b18aa2037a945051f
Sam Abrahams <sjabraha@gmail.com>
2016-02-11 01:56:05 -0800

    Add path separation function to normalize_opts.py
    Also added tests for it. Additionally, moved some assert statements to
    their own helper function.

--- anchorhub/normalization/normalize_opts.py
+++ anchorhub/normalization/normalize_opts.py
@@ -5,6 +5,8 @@ validation and use.
 import os.path as path
 
 from anchorhub.lib.bunch import Bunch
+from anchorhub.util.addsuffix import add_suffix
+from anchorhub.compatibility import get_path_separator
 
 
 def normalize(opts):
@@ -17,6 +19,7 @@ def normalize(opts):
     :return: a namespace with the attributes modified
     """
     opts_dict = vars(opts)
+    ensure_directories_end_in_separator(opts_dict)
     add_abs_path_directories(opts_dict)
     add_open_close_wrappers(opts_dict)
     return Bunch(opts_dict)
@@ -28,7 +31,7 @@ def add_abs_path_directories(opts_dict):
 
     :param opts_dict: dictionary that will be modified
     """
-    assert 'input' in opts_dict and 'output' in opts_dict
+    assert_has_input_output(opts_dict)
     opts_dict['abs_input'] = path.abspath(opts_dict['input'])
     opts_dict['abs_output'] = path.abspath(opts_dict['output'])
 
@@ -45,5 +48,22 @@ def add_open_close_wrappers(opts_dict):
     opts_dict['open'], opts_dict['close'] = opts_dict['wrapper'].split()
 
 
+def ensure_directories_end_in_separator(opts_dict):
+    """
+    Adds a path separator to the end of the input and output directories,
+    if they don't already have them.
 
+    :param opts_dict: dictionary that will be modified
+    """
+    assert_has_input_output(opts_dict)
+    opts_dict['input'] = add_suffix(opts_dict['input'], get_path_separator())
+    opts_dict['output'] = add_suffix(opts_dict['output'], get_path_separator())
 
+def assert_has_input_output(opts_dict):
+    """
+    Helper that asserts that 'input' and 'output' are keys in the dictionary
+
+    :param opts_dict: dictionary to test
+    """
+    assert 'input' in opts_dict
+    assert 'output' in opts_dict
--- anchorhub/normalization/tests/test_normalize_opts.py
+++ anchorhub/normalization/tests/test_normalize_opts.py
@@ -7,12 +7,76 @@ http://www.github.com/samjabrahams/anchorhub/normalization/normalize_opts.py
 import os.path as path
 
 import anchorhub.normalization.normalize_opts as n
+from anchorhub.compatibility import get_path_separator
+
+
+class NormObj(object):
+    """
+    Helper class for testing out normalize_opts.normalize()
+    """
+    def __init__(self, input=None, output=None, wrapper=None, extensions=None,
+                 overwrite = None):
+        if input is not None:
+            self.input = input
+        if output is not None:
+            self.output = output
+        if wrapper is not None:
+            self.wrapper = wrapper
+        if extensions is not None:
+            self.extensions = extensions
+        if overwrite is not None:
+            self.overwrite = overwrite
+
+def test_normalize():
+    """
+    normalize_opts.py: Test normalize()
+    """
+    a = NormObj('.', 'anchorhub-out', '{ }', ['.md'], False)
+    a = n.normalize(a)
+    assert a.input == '.' + get_path_separator()
+    assert a.output == 'anchorhub-out' + get_path_separator()
+    assert a.open == '{'
+    assert a.close == '}'
+    assert a.abs_input == path.abspath(a.input)
+    assert a.abs_output == path.abspath(a.output)
 
 
 def test_abs_path_directories():
-    a = {'input': '.', 'output': 'anchorhub-out'}
+    """
+    normalize_opts.py: Test add_abs_path_directories()
+    """
+    a = {'input': '.',
+         'output': 'anchorhub-out'}
     n.add_abs_path_directories(a)
     assert 'abs_input' in a
     assert 'abs_output' in a
     assert a['abs_input'] == path.abspath('.')
     assert a['abs_output'] == path.abspath('anchorhub-out')
+
+
+def test_add_open_close_wrapper():
+    """
+    normalize_opts.py: Test add_open_close_wrapper()
+    """
+    a = {'wrapper': '[--> <--]'}
+    n.add_open_close_wrappers(a)
+    assert 'open' in a
+    assert 'close' in a
+    assert a['open'] == '[-->'
+    assert a['close'] == '<--]'
+
+
+def test_ensure_directories_end_in_separator():
+    """
+    normalize_opts.py: Test ensure_directories_end_in_separator()
+    """
+    a = {'input': '.', 'output': 'anchorhub-out'}
+    n.ensure_directories_end_in_separator(a)
+    assert a['input'] == '.' + get_path_separator()
+    assert a['output'] == 'anchorhub-out' + get_path_separator()
+
+    b = {'input': 'hello' + get_path_separator(),
+         'output': 'dolly' + get_path_separator()}
+    n.ensure_directories_end_in_separator(b)
+    assert b['input'] == 'hello' + get_path_separator()
+    assert b['output'] == 'dolly' + get_path_separator()
