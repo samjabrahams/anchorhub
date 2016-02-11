0a3091715fea2aeccfeb1a44f7b46b1dd2242383
Sam Abrahams <sjabraha@gmail.com>
2016-02-11 01:20:18 -0800

    Add argument parsing to main.py
    Adds in use of cmdparse.py to parse in command line arguments,
    anchorhub/validation/normalize_opts.py to validate those arguments,
    and anchorhub/normalization/normalize_opts.py to normalize them for use
    later on.
    
    Looks pretty good, I hope!

---
+++ anchorhub/main.py
@@ -0,0 +1,28 @@
+"""
+Command-line entry to AnchorHub, main method is here.
+"""
+
+import anchorhub.cmdparse as cmdparse
+import anchorhub.messages as messages
+import anchorhub.normalization.normalize_opts as normalize_opts
+import anchorhub.validation.validate_opts as validation
+
+
+def main(argv=None):
+    """
+    Main entry method for AnchorHub. Takes in command-line arguments,
+    finds files to parse within the specified input directory, and outputs
+    parsed files to the specified output directory.
+
+    :param argv: a list of string command line arguments
+    """
+    # Get command line arguments, validate them, and normalize them
+    opts = cmdparse.parse_args(argv)
+    assert validation.validate(opts)
+    opts = normalize_opts.normalize(opts)
+
+    # Update client: print input and output directories
+    messages.print_directories(opts)
+
+if __name__ == '__main__':
+    main()
