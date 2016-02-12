bee9373dcf852e7af9f0f1a78dcc17a0922f96fe
Sam Abrahams <sjabraha@gmail.com>
2016-02-12 11:52:35 -0800

    Modify main.py tests to use get_anchorhub_path()

--- anchorhub/tests/test_main.py
+++ anchorhub/tests/test_main.py
@@ -7,10 +7,13 @@ http://www.github.com/samjabrahams/anchorhub/main.py
 from nose.tools import *
 
 import anchorhub.main as main
+from anchorhub.util.getanchorhubpath import get_anchorhub_path
+from anchorhub.compatibility import get_path_separator
 
 
 def test_one():
     """
     main.py: Test defaults with local directory as input.
     """
-    main.main(['.'])
+    main.main([get_anchorhub_path() + get_path_separator() +
+               '../sample/multi-file'])
